9701e21aadf08d13c0bc90ab61d1caac9306ac0e
Gu Lu <gemfeeling@hotmail.com>
2016-12-16 10:03:26 +0800

    trivial changes

--- Assets/PerfAssist/ResourceTracker_Demo/DemoMain.cs
+++ Assets/PerfAssist/ResourceTracker_Demo/DemoMain.cs
@@ -1,14 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Main : MonoBehaviour {
+public class DemoMain : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
         ResourceTracker.Instance = new ResourceTracker(true);
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
